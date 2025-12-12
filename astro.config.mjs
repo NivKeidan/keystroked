0a842dde76fd15ea640f63eb0d2dec65d1d04963
NivKeidan <nivkeidan@gmail.com>
2025-12-12 23:38:20 +0200

    move to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nivkeidan.github.io',
-  base: '/keystroked',
+  site: 'https://keystroked.cheekyservice.com',
   integrations: [react()],
 
   vite: {
